c731c41de64259a482d044496432eb310c513a4d
Ferney Arenas <arenas.mauricio@yahoo.com>
2020-01-14 18:19:50 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,28 +1,43 @@
 def create_an_empty_array
   
+  []
+  
 end
 
 def create_an_array
+  
+  ['United', 'City','Revolution','Sounders']
   
 end
 
 def add_element_to_end_of_array(array, element)
   
+   << ("Atlanta")
+  
 end
 
 def add_element_to_start_of_array(array, element)
+  
+  .unshift ("Mauro")
   
 end
 
 def remove_element_from_end_of_array(array)
   
+  .pop ("Mauro")
+  
 end
 
 def remove_element_from_start_of_array(array)
   
+  [0]
+  
+  
 end
 
 def retrieve_element_from_index(array, index_number)
+  
+  
   
 end
 
